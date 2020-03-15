054d4948eb054d4467301ff9c32d5fb74a6cff3d
Ross Mawdsley <rossmawd@gmail.com>
2020-03-15 14:56:42 +0000

    added 1+1 test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,32 @@
-import { AppComponent } from './app.component';
 
-describe(`App Component`, () => {
-    let component: AppComponent;
-
-    beforeEach(() => {
-        component = new AppComponent();
-    });
-
-    it(`should 1 + 1`, () => {
-        expect(1 + 1).toEqual(2);
-    });
-
-    it(`should have a component`, () => {
-        expect(component).toBeTruthy();
-    });
-
-    it(`should have a title of 'app'`, () => {
-        expect(component.title).toEqual('app');
-    });
+describe('App Component', () => {
+  it('should 1 + 1', () => {
+    expect(1+1).toEqual(4)
+  });
 });
+
+
+// import { AppComponent } from './app.component';
+
+// describe(`App Component`, () => {
+//     let component: AppComponent;
+
+//     beforeEach(() => {
+//         component = new AppComponent();
+//     });
+
+//     it(`should 1 + 1`, () => {
+//         expect(1 + 1).toEqual(2);
+//     });
+
+//     it(`should have a component`, () => {
+//         expect(component).toBeTruthy();
+//     });
+
+//     it(`should have a title of 'app'`, () => {
+//         expect(component.title).toEqual('app');
+//     });
+// });
 
 
 
